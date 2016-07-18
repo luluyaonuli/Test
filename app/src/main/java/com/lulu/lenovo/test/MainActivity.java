1e8444ee44ab7c8b3d2a26530b81e06c467c5713
lulu <820163187@qq.com>
2016-07-18 16:02:03 +0800

    the third version

--- app/src/main/java/com/lulu/lenovo/test/MainActivity.java
+++ app/src/main/java/com/lulu/lenovo/test/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Main", "hahahah");
+        Log.i("Main", "1234567");
     }
 }
